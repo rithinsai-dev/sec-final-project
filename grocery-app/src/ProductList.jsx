f480646d52ce34efe8fee2f4a0fa3dd0ad01a820
rithinsai-dev <rithinsai.pamerla@gmail.com>
2024-11-15 15:26:10 +0530

    boiler plate code
    created a fake json server and checked the api call

---
+++ grocery-app/src/ProductList.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+
+function ProductList() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+      .catch(error => console.error('Error fetching products:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Grocery Products</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            <strong>{product.name}</strong> - {product.price} Rs ({product.quantity} in stock)
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ProductList;
