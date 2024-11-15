ca104d0811e7525801ad36275c62b6cc048454fc
rithinsai-dev <rithinsai.pamerla@gmail.com>
2024-11-15 17:44:53 +0530

    hmm

---
+++ grocery-app/src/Cart.jsx
@@ -0,0 +1,13 @@
+// Cart.jsx
+import React from 'react';
+
+function Cart() {
+  return (
+    <div className="cart-page">
+      <h1>Your Shopping Cart</h1>
+      <p>Your cart is currently empty.</p>
+    </div>
+  );
+}
+
+export default Cart;
